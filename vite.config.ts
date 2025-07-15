2e88df1174804e2b880b993454c63345fa474f3d
Allan <allan@Allans-Air.local>
2025-07-15 01:41:22 -0700

    feat: Enabled publishing to github pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import { ghPages } from 'vite-plugin-gh-pages';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react(), tsconfigPaths()],
+	base: '/har-file-viewer/',
+	plugins: [react(), tsconfigPaths(), ghPages()],
 });
