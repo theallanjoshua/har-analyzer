afbf4ddd801575858c910fd6423d4b7913d85c87
Allan Joshua <allan.joshua0@gmail.com>
2026-03-05 13:13:09 -0800

    ci: Auto derive repo name for correct assets base path when deployed to github pages

--- packages/website/vite.config.ts
+++ packages/website/vite.config.ts
@@ -1,8 +1,23 @@
 import react from '@vitejs/plugin-react';
+import { execSync } from 'node:child_process';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
+function getRepositoryName() {
+	// Get the git origin URL
+	// Eg: https://github.com/user/repo-name.git, git@github.com:user/repo-name.git
+	const originUrl = execSync('git config --get remote.origin.url').toString().trim();
+	const repoParam = originUrl.split('/').at(-1);
+	if (!repoParam?.includes('.git')) {
+		throw new Error('Unexpected git origin URL format');
+	}
+	return repoParam.replace('.git', '');
+}
+
+const repositoryName = getRepositoryName();
+
 // https://vite.dev/config/
 export default defineConfig({
+	base: `/${repositoryName}/`,
 	plugins: [react(), tsconfigPaths()],
 });
