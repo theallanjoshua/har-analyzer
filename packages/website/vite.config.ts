3c2dfd3478172a7d6f56dd3475cf41410ad1c742
Allan Joshua <allan.joshua0@gmail.com>
2026-03-05 13:22:24 -0800

    fix: Handled edge case in github actions when deriving repository name

--- packages/website/vite.config.ts
+++ packages/website/vite.config.ts
@@ -3,15 +3,19 @@ import { execSync } from 'node:child_process';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
+const EXPECTED_REPO_NAME = 'har-analyzer';
+
 function getRepositoryName() {
 	// Get the git origin URL
 	// Eg: https://github.com/user/repo-name.git, git@github.com:user/repo-name.git
 	const originUrl = execSync('git config --get remote.origin.url').toString().trim();
 	const repoParam = originUrl.split('/').at(-1);
-	if (!repoParam?.includes('.git')) {
-		throw new Error('Unexpected git origin URL format');
+	const repoName = repoParam?.replace('.git', '');
+	if (repoName !== EXPECTED_REPO_NAME) {
+		throw new Error(`Unexpected repository name: ${repoName}. Expected: ${EXPECTED_REPO_NAME}.
+			If this was intentional, update the EXPECTED_REPO_NAME constant in vite.config.ts to match the actual repository name.`);
 	}
-	return repoParam.replace('.git', '');
+	return repoName;
 }
 
 const repositoryName = getRepositoryName();
